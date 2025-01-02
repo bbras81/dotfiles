3a5273e9bdd7b54d6e205e0be30d3b6994b4f56d
Bruno Miguel Brás <miguelazevedodg@gmail.com>
2025-01-02 11:16:14 +0000

    Auto-commit: 2025-01-02 11:16:14

---
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -0,0 +1,7 @@
+local base = require("plugins.configs.lspconfig")
+local on_attach = base.on_attach
+local capabilities base.capabilities
+
+local lspconfig = require("lspconfig")
+
+lspconfig.tsserver.setup()
