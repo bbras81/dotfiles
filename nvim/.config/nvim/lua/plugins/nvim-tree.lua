15ebe556d01b7dea4a11632d037db2776d2f5d3d
Bruno Miguel Bras <miguelazevedodg@gmail.com>
2025-02-27 20:05:44 +0000

    i3, nvim plugins

---
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,10 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("nvim-tree").setup({
+      view = { width = 35 },
+      renderer = { icons = { show = { hidden = true } } },
+    })
+  end,
+}
