bc3afe41e6190d060443a191d3c814ae69a9d355
bbras81 <miguelazevedodg@gmail.com>
2025-04-30 17:54:51 +0100

    Auto-commit: 2025-04-30 17:54:51

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,27 @@
 -- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.clipboard = "unnamedplus"
+vim.opt.swapfile = false
+vim.wo.number = true
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Melhor navegação entre janelas
+local map = vim.keymap.set
+map("n", "<C-k>", ":wincmd k <CR>")
+map("n", "<C-j>", ":wincmd j <CR>")
+map("n", "<C-h>", ":wincmd h <CR>")
+map("n", "<C-l>", ":wincmd l <CR>")
+map("n", "<leader>h", ":nohlsearch<CR>", { noremap = true, silent = true })
+
+-- Mover linha para cima/baixo com Alt+j e Alt+k
+map("n", "<A-j>", ":m .+1<CR>==", { noremap = true, silent = true })
+map("n", "<A-k>", ":m .-2<CR>==", { noremap = true, silent = true })
+map("v", "<A-j>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+map("v", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+
+map("n", "<leader>rr", ":CFormat42<CR>")
